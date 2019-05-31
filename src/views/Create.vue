8d75f74353190d3dd05956abed08a81558c31107
Elena Kuklina <liafell.lansera@gmail.com>
2019-05-31 08:14:15 +0400

    .

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -7,10 +7,32 @@
       h1.display-2 Create new
 
       v-layout(column fill-height)
-        .item.elevation-5 1
-          v-text-field
-        .item.elevation-5 2
-        .item.elevation-5 3
+        .item
+          v-layout(row wrap fill-height)
+            v-flex(xs12 sm4)
+              v-text-field.pa-1(label="Name" solo hint="At least 8 characters")
+            v-flex(xs12 sm6)
+              v-text-field.pa-1(label="Short description" solo hint="At least 8 characters")
+            v-flex(xs12 sm2)
+              v-text-field.pa-1(label="Default value" solo hint="At least 8 characters")
+
+        .item
+          v-layout(row wrap fill-height)
+            v-flex(xs12 sm4)
+              v-text-field.pa-1(label="Name" solo hint="At least 8 characters")
+            v-flex(xs12 sm6)
+              v-text-field.pa-1(label="Short description" solo hint="At least 8 characters")
+            v-flex(xs12 sm2)
+              v-text-field.pa-1(label="Default value" solo hint="At least 8 characters")
+
+        .item
+          v-layout(row wrap fill-height)
+            v-flex(xs12 sm4)
+              v-text-field.pa-1(label="Name" solo hint="At least 8 characters")
+            v-flex(xs12 sm6)
+              v-text-field.pa-1(label="Short description" solo hint="At least 8 characters")
+            v-flex(xs12 sm2)
+              v-text-field.pa-1(label="Default value" solo hint="At least 8 characters")
 
 </template>
 
@@ -22,6 +44,7 @@
 .item {
   min-height: 50px;
   min-width: 80px;
-  margin: 10px;
+  margin: 10px 0;
+  background-color: rgba(#000, .1)
 }
 </style>
